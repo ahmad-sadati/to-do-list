87fe2e69486a78211458d0122036890f551f8c9a
Ahmad Sadati <a.sadati336@gmail.com>
2023-11-10 22:35:14 +0330

    fix1

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const taskListShow = document.querySelector(".taskList");
 const taskInput = document.querySelector("#taskInput");
 const date = document.querySelector(".date");
 const taskList = [];
+let currentTaskIndex;
+let oldTaskIndex = '' ;
 $(".date").datepicker({});
 function complete(x) {
   if (taskList[x].done) {
@@ -17,40 +19,48 @@ taskInput.addEventListener("keyup", (e) => {
       title: taskInput.value,
       date: date.value,
       done: false,
-      onEdit: false,
     });
     taskInput.value = "";
     renderList();
   }
 });
+function startEdit(x) {
+  taskInput.value = taskList[x].title;
+  date.value = taskList[x].date;
+  document.getElementById("edit" + (x + 1)).style.display = "none";
+  document.getElementById("save" + (x + 1)).style.display = "inline";
+  console.log(x);
+  if (oldTaskIndex!=='') {
+    console.log(oldTaskIndex);
+    document.getElementById("edit" + (oldTaskIndex + 1)).style.display =
+      "inline";
+    document.getElementById("save" + (oldTaskIndex + 1)).style.display = "none";
+  }
+  oldTaskIndex = x;
+}
+function update (index) {
+  taskList[index].title = taskInput.value
+  taskList[index].date = date.value
+  taskInput.value = ''
+  date.value = ''
+  renderList();
+}
+function remove (index) {
+  taskList.splice(index, 1);
+  renderList()
+}
 function renderList() {
   let helper = "";
   taskList.forEach((val, index) => {
     let myClass = "";
-    let Show = "";
     val.done ? (myClass = "taskDone") : (myClass = "");
-    val.onEdit ? (Show = "d-inline") : (Show = "d-none")
-    helper += `<li class="${myClass}" onclick="complete(${index})" role='button'>${val.title} |
-     ${val.date}</li><button class="" onclick="edit(${index})">Edit</button><button class="${Show}" onclick="SaveEdit(${index})">Save Edit</button><button class="" onclick="remove(${index})">Delete</button>`;
+    helper += `<li class="${myClass}" style="display:block;height:60px" role='button'><span onclick="complete(${index})">${
+      val.title
+    } | ${val.date}</span><span style="float:right"><button id="edit${
+      index + 1
+    }" onclick="startEdit(${index})">Edit</button><button style="display:none" id="save${
+      index + 1
+    }" onclick="update(${index})">save</button><button onclick=remove(${index})>Delete</button></li></span>`;
   });
   taskListShow.innerHTML = helper;
-}
-function edit(index) {
-  let taskEdit = taskList[index];
-  taskInput.value = taskEdit.title;
-  onEdit = true
-  console.log(onEdit);
-  renderList()
-}
-function SaveEdit(index) {
-  let taskEdit = taskList[index];
-  taskEdit.title = taskInput.value;
-  renderList();
-  taskInput.value = "";
-}
-function remove(index) {
-  console.log(index);
-  taskList.splice(index, 1);
-  console.log("ok");
-  renderList();
 }
